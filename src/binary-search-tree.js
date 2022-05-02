67677998e631a92850faf64d7fb9cfb5f4911a6c
lovalik <lovalikk@gmail.com>
2022-05-02 19:09:42 +0300

    4

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,120 +7,173 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-  #root
-
-  constructor() {
-    this.#root = null
-  }
-
-  root() {
-    return this.#root
-  }
-
-  add(data) {
-    if (!this.#root) {
-      this.#root = {
-        data
-      }
-
-      return
-    }
-
-    let currentNode = this.#root
-
-    while (currentNode) {
-      if (data === currentNode.data) {
-        break
-      }
-
-      if (data < currentNode.data) {
-        if (currentNode.leftChild) {
-          currentNode = currentNode.leftChild
-        } else {
-          currentNode.leftChild = {
-            data
-          }
-        }
-      } else {
-        if (currentNode.rightChild) {
-          currentNode = currentNode.rightChild
-        } else {
-          currentNode.rightChild = {
-            data
-          }
-        }
-      }
-    }
-  }
-
-  remove(data) {
-    if (data === this.#root.data) {
+    #root
+  
+    constructor(...values) {
       this.#root = null
-      return
-    }
-
-    let parentNode = null
-    let nodeName = ''
-
-    let currentNode = this.#root
-
-    while (currentNode) {
-      if (data === currentNode.data) {
-        delete parentNode[nodeName]
-        break
+  
+      for (const v of values) {
+        this.add( v )
       }
-
-      parentNode = currentNode
-      nodeName = (data < currentNode.data) ? 'leftChild' : 'rightChild'
-      currentNode = currentNode[nodeName]
     }
-  }
-
-  find(data) {
-    let answer = null
-    let currentNode = this.#root
-
-    while (currentNode) {
-      if (data === currentNode.data) {
-        answer = currentNode
-        break
+  
+    root( ) {
+      return this.#root
+    }
+  
+    add( data ) {
+      if (!this.#root) {
+        this.#root = {
+          data
+        }
+  
+        return
       }
-
-      currentNode = (data < currentNode.data) ? currentNode.leftChild : currentNode.rightChild
+  
+      let currentNode = this.#root
+  
+      while (currentNode) {
+        if (data === currentNode.data) {
+          break
+        }
+  
+        if (data < currentNode.data) {
+          if (currentNode.leftChild) {
+            currentNode = currentNode.leftChild
+          } else {
+            currentNode.leftChild = {
+              data
+            }
+          }
+        } else {
+          if (currentNode.rightChild) {
+            currentNode = currentNode.rightChild
+          } else {
+            currentNode.rightChild = {
+              data
+            }
+          }
+        }
+      }
     }
-
-    return answer
-  }
-
-  has(data) {
-    return Boolean(this.find(data))
-  }
-
-  min() {
-    let currentNode = this.#root
-    let answer = currentNode ? currentNode.data : null
-
-    while (currentNode && currentNode.leftChild) {
-      answer = currentNode.leftChild.data
-      currentNode = currentNode.leftChild
+  
+    #removeFrom(data, currentNode, parentNode = null) {
+      let nodeName = ''
+  
+      while (currentNode) {
+        if (data === currentNode.data) {
+  
+          // вариант нет детей
+          if (!currentNode.leftChild && !currentNode.rightChild) {
+            if (parentNode) {
+              delete parentNode[nodeName]
+            } else {
+              this.#root = null
+            }
+            break
+          }
+  
+          // вариант с одним
+          if ((currentNode.leftChild && !currentNode.rightChild) || (!currentNode.leftChild && currentNode.rightChild)) {
+            const replacementNode = currentNode.leftChild || currentNode.rightChild
+            if (parentNode) {
+              parentNode[nodeName] = replacementNode
+            } else {
+              this.#root = replacementNode
+            }
+            break
+          }
+  
+          // вариант с двумя
+          const replacementNode = this.#findFrom(
+            this.#minFrom( currentNode.rightChild ),
+            currentNode.rightChild
+          )
+  
+          this.#removeFrom( replacementNode.data, currentNode.rightChild, parentNode )
+  
+          if ( currentNode.leftChild ) {
+            replacementNode.leftChild = currentNode.leftChild
+          }
+  
+          if ( currentNode.rightChild !== replacementNode ) {
+            replacementNode.rightChild = currentNode.rightChild
+          }
+  
+          if ( parentNode ) {
+            parentNode[ nodeName ] = replacementNode
+          } else {
+            this.#root = replacementNode
+          }
+  
+          currentNode.leftChild = null
+          currentNode.rightChild = null
+        }
+  
+        parentNode = currentNode
+        nodeName = ( data < currentNode.data ) ? 'leftChild' : 'rightChild'
+        currentNode = currentNode[ nodeName ]
+      }
     }
-
-    return answer
-  }
-
-  max() {
-    let currentNode = this.#root
-    let answer = currentNode ? currentNode.data : null
-
-    while (currentNode && currentNode.rightChild) {
-      answer = currentNode.rightChild.data
-      currentNode = currentNode.rightChild
+  
+    remove( data ) {
+      this.#removeFrom( data, this.#root )
     }
-
-    return answer
+  
+    #findFrom( data, currentNode ) {
+      let answer = null
+  
+      while ( currentNode ) {
+        if ( data === currentNode.data ) {
+          answer = currentNode
+          break
+        }
+  
+        currentNode = ( data < currentNode.data ) ? currentNode.leftChild : currentNode.rightChild
+      }
+  
+      return answer
+    }
+  
+    find( data ) {
+      return this.#findFrom( data, this.#root )
+    }
+  
+    has( data ) {
+      return Boolean(this.find( data ))
+    }
+  
+    #minFrom( currentNode ) {
+      let answer = currentNode ? currentNode.data : null
+  
+      while (currentNode && currentNode.leftChild) {
+        answer = currentNode.leftChild.data
+        currentNode = currentNode.leftChild
+      }
+  
+      return answer
+    }
+  
+    min() {
+      return this.#minFrom( this.#root )
+    }
+  
+    #maxFrom( currentNode ) {
+      let answer = currentNode ? currentNode.data : null
+  
+      while ( currentNode && currentNode.rightChild ) {
+        answer = currentNode.rightChild.data
+        currentNode = currentNode.rightChild
+      }
+  
+      return answer
+    }
+  
+    max() {
+      return this.#maxFrom( this.#root )
+    }
+  
   }
-
-}
 
 module.exports = {
   BinarySearchTree
